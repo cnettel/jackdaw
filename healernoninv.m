19ac5cd728546b069f0502280896d2eba079e797
Carl Nettelblad <carl.nettelblad@icm.uu.se>
2018-03-02 16:00:51 +0100

    Major cleanup

--- healernoninv.m
+++ healernoninv.m
@@ -50,19 +50,10 @@ for outerround=1:numrounds
     patternmask = (pattern < 0 | isnan(pattern));
     truebild = pattern;
     truebild(patternmask) = x(patternmask) + lambdas(patternmask);
-    filter = 1./sqrt(max(truebild(:)+1,1e-15)) + 0.00001;
-    filter = 1./filter;
-    %filter = filter + sqrt(0.1);
-    %filter(:) = 1;
-    filter = filter .* (1 - (1 - 1./sqrt(relmumasked)) * patternmask);
-    filter(:) = 1;
     filter = hann(side2, 'periodic');
     filter = filter * filter';
     filter = filter + 1e-7;
-    filter(filter<0) = 0;
     filter = fftshift(filter);
-    filter = filter + filter';
-    filter = filter .* side2 .* side2 ./ sum(filter(:));
     filter = reshape(filter,side2*side2,1);
     filterorig = filter;
     global f2;
@@ -91,126 +82,15 @@ for outerround=1:numrounds
     l2 = l;
     %l = l - 1e-1;
     %u = u + 1e-1;
-%% Positive real
-    l(1:65536) = -diffxt(1:65536);
-    
-    
-    
-    global l99;
-    l99 = l;
-    global projector;
-    %projector = proj_box(l, u);
-    %projector = proj_l2(1, spdiags(diffxt,1:(side2*side2), side2*side2, side2*side2), x2 > 0);
-    proxector = prox_boxDual(l, u, -1);
-    
-    
-    %l2 = ourlinpflat(addlevel .* filterorig(:), 2);
-
+          
     l2(isinf(l2)) = 0;
-    global addlevel;
-
     addlevel = ourlinp(l2,1);
-    addlevel2 = ourlinpflat(addlevel .* filterorig(:), 2);
-    AAA = norm(l2)
-    norm(addlevel2)
 
-    l(:) = -0.1;
-    u(:) =  0.1;
-    u(x2 > 0) = maxdiff;
-    l(x2 > 0) = -maxdiff;
-    % Just tighten it a little bit
-    l = min(-l2*(1-1e-6),l);
-    u = max(-l2*(1-1e-6),u);
-    projector2 = proj_box(l, u);
-    
-    %z2 = ourlinp(z0,2);
-    %[val, z3] = projector2(z2, 0);
-    %z0 = ourlinp(z3, 1) ./ filter;
-    filter(:) = 1;
-    global smoothop
-    smoothop = diffpoisson(filter, pattern(:), (diffx(:) + lambdas(:)).* 1 ./ filter, lambdas(:) * 1./filter, diffx .* 1./filter);
-    desired = max(0,diffxorig);
-    %z0 = findfixpoint(smoothop, z0, betanow, 1e4, desired - diffx);
-    %[val, z11] = projector(ourlinp(z0,2),0);
-    %z0 = ourlinp(z11, 1) ./ filter;
-    global z19;
-    z19 = z0;
-    global z12;
-    %[throwaway,z12] = smoothop(-z0/betanow, 1/betanow);
-    %throwaway
-    
-    %smoothop = diffpoisson(filter, pattern(:), (diffx(:) + lambdas(:)).* 1 ./ filter, lambdas(:) * 1./filter, z12 + diffx(:) .* 1./filter); 
-    %global z13;
-    %[throwaway,z13] = smoothop(-z0/betanow, 1/betanow);
-    %throwaway
-    
-    global z14;
-    z14=diffx;
-    global z15;
-    z15 = smoothop;
-   
-    
-    
-    'KORREKT29'
-%    [x,out] = tfocs_SCD([], {ourlinp,0;linop_scale(1),0 }, {prox_dualize2(smoothop,'negative',-z0{1}), prox_dualize2(projector,'negative',-z0{1})}, betanow, 0*ourlinp(desired,2)-0*diffxt+0*ourlinp((truebild(:)-diffx(:)-lambdas(:))./filter,2), z0, opts, copts);
-    %[x,out] = tfocs_SCD(smoothop, {linop_adjoint(ourlinp);0*zeros(1048576,1)}, {proxector}, betanow, zeros(1048576, 1), z0, opts, copts);
-    %[x,out] = tfocs(smoothop, {ourlinp;0*zeros(1048576,1)}, projector, zeros(2097152,1), opts);
-    size(addlevel)
-    norm(addlevel)
-  
-    zzz = smooth_quad_hack2(1e8 .* (x22 <= 0), -diffxt-l2);
-    ppp = zzz(-l2)
-    %PPP = sum((-l2.^2).* (x2 <= 0))
-    len2 = length(x2);
-    
-    
-    %[x,out] = tfocs({smoothop; smooth_quad(1e-9)}, {ourlinp,addlevel; linop_compose(linop_dot(double(pattern>=0)), ourlinp), sum(addlevel(pattern>=0)-pattern(pattern>=0)+diffx(pattern>=0))}, smooth_quad_hack2(1e7 .* (x22 <= 0), -diffxt-l2), -l2 * 1, opts);
+    factor = 1;
+    smoothop = diffpoisson(factor, pattern(:), (diffx(:) + lambdas(:)).* 1 ./ factor, lambdas(:) * 1./factor, diffx ./ factor);
     [x,out] = tfocs({smoothop}, {ourlinp,addlevel}, smooth_quad_hack2(1e18 .* (x22 <= 0), -diffxt-l2), -l2 * 1, opts);
-%[x,out] = tfocs(smoothop, {ourlinp,addlevel}, projector2, -l2, opts);
-
-
-
-
-    %global z9;
-    %z9 = ourlinpflat(x,1);% ./ filterorig;
-    %global z10;
-    %[throwaway, z10] = smoothop(-out.dual/1e-15,1/1e-15);
-    %nz9 = norm(z9)
-    %nzdiff = norm(z9-z10)
-    %opts.maxIts = opts.maxIts * 4;
-    %[x2,out2] = tfocs_SCD(proj_box(l, u), {ourlinp}, prox_dualize(smoothop,'negative'), betanow / 2, ourlinp((truebild(:)-diffx(:)-lambdas(:))./filter,2), z9, opts, copts);
-    %z0 = out.dual;
-    %z9 = out2.dual;
-
-    %[val, x] = projector(ourlinp(z10,2), 0);
-    %x = ourlinp(x,1) ./ filter;
-    %global z11;
-    %z11 = x;
-    %x = z9;
-    
-    %x2 = ourlinp(x2,1) ./ filter;
-    %x(patternmask) = x(patternmask) + (x2(patternmask) - x(patternmask) / 0.5);
-
-    % Return translation
-    global z99
-    z99 = x;
-    x = ourlinp(x,1) + addlevel;%./ filterorig(:) + addlevel;
-    global z100
-    z100 = x;
-    size(x)
-    size(diffx)
-    newx = -1;
-    %while any(newx <= -0.005 + 2*eps(max(abs(newx))))
-    %  newx = x + diffx(:);
-    %  x = x * 0.99 + 2 * eps(max(abs(newx)));
-    %  'Dip'
-    %end  
-    %x = newx;
-    %[skip, newx] = smoothop(x,1e-15);
+    x = ourlinp(x,1) + addlevel;
     x = x + diffx(:);
-    %x = x + 2 * eps(max(abs(x)));
-    
-    betanow = betanow * copts.muDecrement;
 end
 
 outpattern = reshape(x,side2,side2);
