667522665b85cedcb5707e685f81a960639942fc
Carl Nettelblad <carl.nettelblad@icm.uu.se>
2018-04-15 11:04:16 +0200

    Remove all non-coacs parts

--- sizer/plogl.m
+++
@@ -1,7 +0,0 @@
-function [ logl ] = plogl( bilden, pc)
-mask = bilden < 1e-9;
-bilden = bilden .* (1-mask) + mask .* 1e-9; 
-terms = bilden .* pc - bilden;
-logl = sum(terms(:)) + sum(mask(:)) * 1e9 - bilden(mask);
-end
-
