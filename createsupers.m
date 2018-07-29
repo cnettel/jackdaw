13b720f186c741fa56115e866738f6c32fa38eb9
Carl Nettelblad <carl.nettelblad@icm.uu.se>
2018-07-29 23:44:53 +0200

    Updated createsupers, not using all

--- createsupers.m
+++ createsupers.m
@@ -1,4 +1,7 @@
 vs72super = h5read('vs72/phasing.h5','/super_images');
 rs72f2super = h5read('rs72f2/phasing.h5','/super_images');
+%vs72nwsuper = h5read('vs72nw/phasing.h5','/super_images');
+rs72super = h5read('rs72/phasing.h5','/super_images');
 
-save -v7.3 superimages vs72super rs72f2super
+save -v7.3 superimages vs72super rs72f2super rs72super
+%vs72nwsuper rs72super
